8e0c4f2b28485b67ed64bbb1873c5c6f42f919ce
Leo Breuer <contact@cheetahbyte.dev>
2023-07-14 12:02:49 +0200

    feat: jwt for verification token

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from base_service import Service
 from tortoise import Tortoise, run_async
 from models import User, Session
-from utils import SessionRediser
+from utils import SessionRediser, Rediser, verify_verification_token, create_verification_token
 from argon2 import hash_password, verify_password
+import uuid
 
 async def on_user_password_create(data: dict):
     user = await User(
@@ -12,7 +13,8 @@ async def on_user_password_create(data: dict):
         id=data["id"],
     )
     await user.save()
-    return {"id": str(user.id)}
+    vid = create_verification_token(user.id)
+    return {"id": str(user.id), "vid": str(vid)}
 
 
 async def on_user_password_update(data: dict):
@@ -25,6 +27,7 @@ async def on_user_login(data: dict) -> dict:
     logged_in: bool = verify_password(user.password_hash.encode("utf-8"), password)
     session = await Session.create(user=user.id, ip=str(data["ip"]))
     await (await session_redis.init()).new(session.id, user.id, 172800)
+    #
     return {"id": str(session.id)}
 
 async def on_user_session_exists(data: dict) -> bool:
@@ -46,6 +49,7 @@ async def init():
 
 if __name__ == "__main__":
     session_redis = SessionRediser("redis://192.168.1.28")
+    verfication_redis = Rediser("redis://192.168.1.28", 1)
     auth_service = Service("auth", "amqp://guest:guest@192.168.1.28")
     auth_service.add_event_handler("auth.password.initial", on_user_password_create)
     auth_service.add_event_handler("auth.password.change", on_user_password_update)
