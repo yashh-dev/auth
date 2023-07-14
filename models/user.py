ee0d3b07976605a5d05b34b9b7509e77498bb99d
Leo Breuer <contact@cheetahbyte.dev>
2023-07-14 12:03:26 +0200

    feat(models): add verification to user model

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@ from tortoise import fields, Model
 class User(Model):
     id = fields.UUIDField(pk=True)
     password_hash = fields.CharField(max_length=256)
+    verified = fields.BooleanField(default=False)
     modified_at = fields.DatetimeField(auto_now=True)
     created_at = fields.DatetimeField(auto_now_add=True)
 
