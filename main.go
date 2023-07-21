2c3175be25a58e99ddb82f6655a9c6574c2f7cd1
Leo Breuer <contact@cheetahbyte.dev>
2023-07-21 20:25:23 +0200

    feat(verification): add listener for `auth.verify`
    Closes #4

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
-import (
-	"miauw.social/auth/handlers"
-)
+import "miauw.social/auth/handlers"
 
 func main() {
 	var forever chan struct{}
 	go Serve("auth.initial", handlers.UserCreate)
 	go Serve("auth.login", handlers.UserLogin)
+	go Serve("auth.verify", handlers.UserVerify)
 	<-forever
 }
