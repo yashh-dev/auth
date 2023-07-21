78f5435cd05ddcc8dafaadb403412c692b53533d
Leo Breuer <contact@cheetahbyte.dev>
2023-07-21 19:37:29 +0200

    refactor(handler|create): move function to other file

---
+++ handlers/create.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"encoding/json"
+
+	"github.com/gofrs/uuid"
+	"miauw.social/auth/database"
+	"miauw.social/auth/database/models"
+	"miauw.social/auth/security"
+)
+
+type UserCreateData struct {
+	ID       string
+	Password string
+}
+
+func UserCreate(rawData []byte) (Response, error) {
+	var userCreateData UserCreateData
+	err := json.Unmarshal(rawData, &userCreateData)
+	if err != nil {
+		return Response{
+			Content: nil,
+			Status: ResponseStatus{
+				Code:   422,
+				Title:  "Not processable!",
+				Detail: "The data send to the worker was not processable.",
+				Type:   "https://auth.miauw.social/login/not-processable",
+			},
+		}, err
+	}
+	passwordHash, err := security.EncryptPassword(userCreateData.Password)
+	db := database.Conn()
+	account := models.Account{PasswordHash: passwordHash, Base: models.Base{ID: uuid.FromStringOrNil(userCreateData.ID)}}
+	db.Create(&account)
+	return Response{
+		Content: nil,
+		Status: ResponseStatus{
+			Code: 201,
+		},
+	}, err
+}
