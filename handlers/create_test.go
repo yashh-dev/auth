56a219fca3015f0099449385e34a3ca96e073b73
yashh-dev <yashwanthkrishna5027@gmail.com>
2023-08-13 23:02:19 +0530

    minor:(create_user) added positive and negative case

--- handlers/create_test.go
+++ handlers/create_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -9,10 +10,24 @@ import (
 	"gorm.io/gorm"
 )
 
-func TestCreate(t *testing.T){
-		rawData := []byte(`{"id":"945123486","password":"test@123"}`)
+
+
+
+ func TestCreate(t *testing.T){
+
+	validaUserData := UserCreateData{
+		ID: "894cf31c-cb98-45f0-bce8-63848e09689e",
+		Password: "test@123",
+	}
+
+	validaUserDataRaw ,err:=json.Marshal(validaUserData)
+	if err != nil{
+		t.Error(err)
+	}
+	invalidUserDataRaw := []byte(`id:894cf31c-cb98-45f0-bce8-63848e09689e,password:test@123`)
 	
-		db, mock, err := sqlmock.New()
+
+	db, mock, err := sqlmock.New()
 		if err != nil {
 			t.Fatalf("error on '%s' stub database connection", err)
 		}
@@ -26,19 +41,30 @@ func TestCreate(t *testing.T){
 			t.Error("gorm.Err",err)
 		}
 	
+	t.Run("user data inserts into the db and token recieved",func(t *testing.T) {
+
 		mock.ExpectBegin()
 		mock.ExpectExec("INSERT INTO `accounts` .`id`,`deleted_at`,`password_hash`,`verified`. VALUES (.+)").WillReturnResult(sqlmock.NewResult(1, 1))
 		mock.ExpectCommit()
 
-		response,err:=UserCreate(gormDb,rawData)
+		response,err:=UserCreate(gormDb,validaUserDataRaw)
 
-		if err != nil{
-			t.Error("handler.create",err)
-		}
+		assert.Empty(t,err)
 
 		if err := mock.ExpectationsWereMet(); err != nil {
 			t.Errorf("there were unfulfilled expectations: %s", err)
 		}
 
 		assert.Equal(t,201,response.Status.Code)
+		assert.NotEmpty(t,response.Content)
+	})
+	
+	t.Run("invalid user data fails with code 422",func(t *testing.T) {
+		response,err:=UserCreate(gormDb,invalidUserDataRaw)
+
+		if err != nil{
+			t.Log("handler.create",err)
+			assert.Equal(t,422,response.Status.Code)
+		}
+	})
 }
